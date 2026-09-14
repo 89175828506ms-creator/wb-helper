374d41fb663afd41051f6216fd67a1dfb0fb08df
89175828506ms-creator <89175828506ms@gmail.com>
2026-09-14 22:01:18 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,29 @@
+# Настройки Wildberries (2 кабинета)
 WB_ACCOUNTS = [
     {
         "name": "WB Магазин 1",
-        "token": "eyJhbGciOiJFUzI1NiIsImtpZCI6IjIwMjYwOTAzdjEiLCJ0eXAiOiJKV1QifQ.eyJhY2MiOjEsImVudCI6MSwiZXhwIjoxODA1MTc1NDY2LCJpZCI6IjAxYTBhMGZmLTE1YzktN2JjMy1iMjQ4LTJjNmY4OTNmYzI0NSIsImlpZCI6MTMwNTc2NzU3LCJvaWQiOjQxODA3NTUsInMiOjE2LCJzaWQiOiIzMzkzNjBhNS04ZjBhLTRlMTktOGVjZC1jY2NmMjNjNTk2YmUiLCJ0IjpmYWxzZSwidWlkIjoxMzA1NzY3NTd9.XvGXlrbr98IrFGeXyp5heFdLuTNWZklkzBN9YE1tMRMJRPyMHaZvxiYJ-VUupa8SLaNYPJ6P55_92fJd5GrNyA",
+        "token": "EyJhbGciOiJFUzI1NiIsImtpZCI6IjIwMjYwOTAzdjEiLCJ0eXAiOiJKV1QifQ.eyJhY2MiOjEsImVudCI6MSwiZXhwIjoxODA1MTc1NDY2LCJpZCI6IjAxYTBhMGZmLTE1YzktN2JjMy1iMjQ4LTJjNmY4OTNmYzI0NSIsImlpZCI6MTMwNTc2NzU3LCJvaWQiOjQxODA3NTUsInMiOjE2LCJzaWQiOiIzMzkzNjBhNS04ZjBhLTRlMTktOGVjZC1jY2NmMjNjNTk2YmUiLCJ0IjpmYWxzZSwidWlkIjoxMzA1NzY3NTd9.XvGXlrbr98IrFGeXyp5heFdLuTNWZklkzBN9YE1tMRMJRPyMHaZvxiYJ-VUupa8SLaNYPJ6P55_92fJd5GrNyA"
     },
     {
         "name": "WB Магазин 2",
-        "token": "eyJhbGciOiJFUzI1NiIsImtpZCI6IjIwMjYwOTAzdjEiLCJ0eXAiOiJKV1QifQ.eyJhY2MiOjEsImVudCI6MSwiZXhwIjoxODA1MTc1NDk3LCJpZCI6IjAxYTBhMGZmLThjZTEtNzYxOS05NTc4LTQ0YWQ3OTVkZjlkYyIsImlpZCI6Mjg0MzkyMTgsIm9pZCI6MjgzMTgzLCJzIjoxNiwic2lkIjoiZWQxMzlhMzMtMmFmOS00N2YzLTgwOGItZGI4ZDNhOTFhZTBiIiwidCI6ZmFsc2UsInVpZCI6Mjg0MzkyMTh9.E867AnfxN_avZBfgExF0aXDmHGNvQjcXPJMVaAdd9y8SQnkSRLfe9uUoNRca-FUBUTEL9XiA0amJL5flRcLMjQ",
+        "token": "eyJhbGciOiJFUzI1NiIsImtpZCI6IjIwMjYwOTAzdjEiLCJ0eXAiOiJKV1QifQ.eyJhY2MiOjEsImVudCI6MSwiZXhwIjoxODA1MTc1NDk3LCJpZCI6IjAxYTBhMGZmLThjZTEtNzYxOS05NTc4LTQ0YWQ3OTVkZjlkYyIsImlpZCI6Mjg0MzkyMTgsIm9pZCI6MjgzMTgzLCJzIjoxNiwic2lkIjoiZWQxMzlhMzMtMmFmOS00N2YzLTgwOGItZGI4ZDNhOTFhZTBiIiwidCI6ZmFsc2UsInVpZCI6Mjg0MzkyMTh9.E867AnfxN_avZBfgExF0aXDmHGNvQjcXPJMVaAdd9y8SQnkSRLfe9uUoNRca-FUBUTEL9XiA0amJL5flRcLMjQ"
     }
 ]
+
+# Настройки Ozon (1 кабинет)
+OZON_ACCOUNTS = [
+    {
+        "name": "Ozon 1",
+        "client_id": "412290",
+        "api_key": "616e94c6-18d6-4884-8ca8-add97f9f90a5"
+    }
+]
+
+# Настройки почты Mail.ru (отправка на mebel_2026@bk.ru)
+EMAIL_SETTINGS = {
+    "smtp_server": "smtp.mail.ru",
+    "smtp_port": 465,
+    "sender_email": "mebel_2026@bk.ru",
+    "app_password": "AFJyEXSvtK9TqLvGiwkb",
+    "receiver_email": "mebel_2026@bk.ru"
+}
